2cb066a36047956807ec3c75583e80ff965f113f
charls <1842872290@qq.com>
2018-04-21 22:32:10 +0800

    test

---
+++ game.py
@@ -0,0 +1,19 @@
+
+
+
+import socketserver
+
+class foo:
+
+    def __init__(self, name):
+        self.name = name
+
+    def n(self):
+        print(self, self.name)
+
+
+a = foo('sada')
+a.n()
+
+
+
